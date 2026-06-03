5f61efe5b286f37a082201925987d9f096b04bb9
IsaacGivi <isaacruizse@gmail.com>
2026-06-03 05:11:52 +0200

    add Initial commit, gitignore, plantilla, and add recover password view

--- README.md
+++
@@ -1,2 +0,0 @@
-# PrograAvanzadaWEB_Practica1
-Repositorio para el desarrollo de la práctica 1 para Programación Avanzada en Web
---
+++ VitalApp_WEB/VitalApp_WEB/Controllers/RecuperarContrasenaController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace VitalApp_WEB.Controllers
+{
+    public class RecuperarContrasenaController : Controller
+    {
+        public IActionResult Paso1() => View();
+
+        public IActionResult Paso2() => View();
+
+        public IActionResult Paso3() => View();
+    }
+}
