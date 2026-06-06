284590e4e81c0200773b9d5d6dcb2f7d2946d51d
falfaro20310-art <falfaro20310@ufide.ac.cr>
2026-06-05 23:49:28 -0600

    Commit vista Registrar
    Se desarrolla la vista de registrar usuario

--- VitalApp_WEB/VitalApp_WEB/Controllers/HomeController.cs
+++ VitalApp_WEB/VitalApp_WEB/Controllers/HomeController.cs
@@ -15,6 +15,10 @@ namespace VitalApp_WEB.Controllers
         {
             return View();
         }
+        public IActionResult Registro()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
