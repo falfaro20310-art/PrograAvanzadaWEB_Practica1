7e253743f506ce68cc8ff1bb1730b7785de3951e
FauricioCespedes <108296043+FauricioCespedes@users.noreply.github.com>
2026-07-21 20:40:19 -0600

    Add general config and NuGet packages to web and api projects

--- VitalApp_WEB/VitalApp_API/Program.cs
+++ VitalApp_WEB/VitalApp_API/Program.cs
@@ -1,20 +1,44 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+//using VitalApp_API.Services;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
+//builder.Services.AddScoped<ITest, TestService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]!)),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
+//Middleware de Errores
+//app.UseExceptionHandler("/api/Error/RegistrarError");
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
--- VitalApp_WEB/VitalApp_WEB/Program.cs
+++ VitalApp_WEB/VitalApp_WEB/Program.cs
@@ -3,15 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Dependencias
+builder.Services.AddHttpClient();
+builder.Services.AddSession();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//Middleware de Errores
+//app.UseExceptionHandler("/Error/CapturarError");
+
+app.UseSession();
+
+app.UseHsts();
 
 app.UseHttpsRedirection();
 app.UseRouting();
